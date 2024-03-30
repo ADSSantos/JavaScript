5fc76a77a07bc12073cfc67de6a8c059396b40de
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-03-30 17:58:47 +0000

    Condição if else aninhada

---
+++ Modulo D/condição if else/exercicios/exe 03/exe03.js
@@ -0,0 +1,19 @@
+var idade = 89
+console.log(`você tem ${idade} anos`)
+if (idade < 16) {
+    console.log('nao pode votar')
+
+} else if( idade < 18  || idade > 65) {
+    
+        console.log('voto opcional')
+   
+     } else { 
+        console.log('voto obrigatorio')
+
+       
+            
+        
+     }
+
+
+   
