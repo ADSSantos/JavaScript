231182992550d5c2ae4ecc54d8a1620836f06e01
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-04-11 13:59:48 +0100

    appCalcular idade

--- Modulo D/Condição Multiplas/exercicios/App qual seu ano de nascimento/cod.js
+++ Modulo D/Condição Multiplas/exercicios/App qual seu ano de nascimento/cod.js
@@ -1,23 +1,41 @@
-const idadeInput = document.getElementById("idade")
+function calcular(){
+   let data = new Date()
+   let ano = data.getFullYear()
+   let formulario = document.getElementById("idade")
+   let result = document.getElementById("result")
 
+   if (formulario.value.length == 0 || formulario.value > ano) {
+        window.alert("[ERROR] Invalid")
+   } else {
+    let sexo = document.getElementsByName("sexo")
+    let idade = ano - Number(formulario.value)
+    let genero = ""
 
-function calcularIdade() {
-    let sexoSelecionado = document.querySelector('input[name="sexo"]:checked')
-    let anoNascimento = parseInt(document.getElementById("idade").value);
-    let dataAtual = new Date();
-    let anoAtual = dataAtual.getFullYear();
-    let result = document.getElementById("result");
-    let idade = anoAtual - anoNascimento;
+    let img = document.createElement("img")
+    img.setAttribute("id", "foto")
+    if (sexo [0].checked) {
+        genero = "Homem"
+        if ( idade >= 0 && idade <= 10){
+            //criança
+            img.setAttribute("src","criança.jpg")
 
-  
-    if (sexoSelecionado) {
-        let sexo = sexoSelecionado.id ==="sexo_m" ? "Masculino" : "Feminino";
-        result.innerHTML = "Você tem " + idade + " anos sexo " + sexo ;
-    } else {
-        result.innerHTML = "Você tem " + idade + " anos";
+        } else if (idade  < 21){
+            //jovem
+
+        } else if (idade > 50) {
+            //adulto
+
+        } else{
+                //idoso
+        }
+    } else if(sexo [1].checked) {
+        genero = "Mulher"
     }
+    result.innerHTML =  `Detectamos ${genero} com ${idade} anos`
+    result.appendChild(img)
+    img.style.marginTop = "60px";
+   }
 }
-
 
 
 
