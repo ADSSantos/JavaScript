006fa62f2503dee86c296abbb8603ad78da795ad
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-04-25 18:39:01 +0100

    1 a 10 com FOR loop

---
+++ Modulo E Repetição/For/Exercicios/node_3884eb25b10dc.tmp.js
@@ -0,0 +1,11 @@
+
+// Exercicio criar um ALGORITIMO contador de 1 ate 10, com laço de reptição com FOR.
+
+//1 Nesta extrutura iniciamos o algoritimo com a declaração da variavel .
+//2 Logo apos o algoritimo pssa pela faze de testar a condição .
+//3 Terceira parte é o incremento neste caso vai somar mais 1 enquanto o teste for verdadeiro!
+
+for(let cont = 10 ; cont > 1 ; cont++) {
+    console.log(cont)
+
+}
