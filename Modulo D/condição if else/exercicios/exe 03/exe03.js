6f88dabdf1db7b3d3b0636277dfae545b88ea759
ADSSantos <121822984+ADSSantos@users.noreply.github.com>
2024-04-25 09:08:39 +0100

    versão 1 quiz game inglish

---
+++ Modulo D/condição if else/exercicios/exe 03/exe03.js
@@ -0,0 +1,27 @@
+let resp = "beber"
+
+
+switch(resp) {
+    case "to be":
+        console.log("boa!!")
+        break
+
+    case "correr":
+        console.log("xiiiii!!")
+        break
+
+    case "comer":
+        console.log("hummm!!")
+        break
+
+    case "beber":
+        console.log("vai mau!!")
+        break
+
+}
+
+
+
+
+
+
